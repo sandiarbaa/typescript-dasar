abf78c3b60d8ceb242f864eac02fe9083d5ef4fc
sandiarbaa <sandiarbaa@gmail.com>
2024-04-14 12:59:39 +0700

    test: Verify TypeScript compilation validity against Jest tests

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -1,12 +1,16 @@
 describe("Data Type", function () {
   it("should must declare", function () {
-    const name: string = "Sandi Arba Putra";
-    const balance: number = 1000000;
-    const isVip: boolean = true;
+    let name: string = "Sandi Arba Putra";
+    let balance: number = 1000000;
+    let isVip: boolean = true;
 
     // console.info(name, balance, isVip);
     console.info(name);
     console.info(balance);
     console.info(isVip);
+
+    // name = 100;
+    // balance = "sandi";
+    // isVip = 100;
   });
 });
