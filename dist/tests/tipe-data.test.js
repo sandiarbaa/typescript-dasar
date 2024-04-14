59aef0e79945be7526a4f202d692dad8de782946
sandiarbaa <sandiarbaa@gmail.com>
2024-04-14 12:43:24 +0700

    add(typescript): Add tipe-data file for primitive data type

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,12 @@
+"use strict";
+describe("Data Type", function () {
+    it("should must declare", function () {
+        const name = "Sandi Arba Putra";
+        const balance = 1000000;
+        const isVip = true;
+        // console.info(name, balance, isVip);
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,12 @@
+describe("Data Type", function () {
+  it("should must declare", function () {
+    const name: string = "Sandi Arba Putra";
+    const balance: number = 1000000;
+    const isVip: boolean = true;
+
+    // console.info(name, balance, isVip);
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+  });
+});
