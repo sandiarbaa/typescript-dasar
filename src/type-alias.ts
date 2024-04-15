906ee47f00d350598d8127265286ebe637337908
sandiarbaa <sandiarbaa@gmail.com>
2024-04-15 21:10:11 +0700

    add: using Optional Properties in Typescript

--- src/type-alias.ts
+++ src/type-alias.ts
@@ -5,11 +5,14 @@ export type ID = string | number;
 export type Category = {
   id: ID;
   name: string;
+  description?: string; // ini contoh optional properties
 };
+// jadi kita bisa buat properti dari object ini tidak wajib di isi
 
 export type Product = {
   id: ID;
   name: string;
   price: number;
   category: Category;
+  description?: string;
 };
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -2,11 +2,11 @@ describe("Object", () => {
   it("should support in typescript", () => {
     // jadi ini membuat object secara langsung di ts tanpa menggunakan type alias dulu, jika objectnya hanya sederhana
     // kalau objectnya kompleks, misal properti nya lebih dari 10 atau 20, lebih baik gunakan type alias
-    const person: { id: string; name: string } = {
+    const person: { id: string; name: string; hobbies?: string[] } = {
       id: "1",
       name: "Sandi",
     };
-
+    // optional properties, jadi cukup tambahkan ? di atribut atau propertiesnya
     console.info(person);
 
     person.id = "2";
