4bbb158fd3a6e0a3676dab0e55f95270f64e4be8
sandiarbaa <sandiarbaa@gmail.com>
2024-04-17 12:35:34 +0700

    add: If Statement

--- dist/tests/function.test.js
+++ dist/tests/function.test.js
@@ -1,57 +1,78 @@
 "use strict";
 describe("Function", () => {
-  it("should support in typescript", () => {
-    function sayHello(name) {
-      return `Hello ${name}`;
-    }
-    expect(sayHello("Sandi")).toBe("Hello Sandi");
-    function printHello(name) {
-      console.info(`Hello ${name}`);
-    }
-    printHello("Sandi");
-  });
-  it("should support default value", () => {
-    function sayHello(name = "Guest") {
-      return `Hello ${name}`;
-    }
-    expect(sayHello()).toBe("Hello Guest");
-    expect(sayHello("Sandi")).toBe("Hello Sandi");
-  });
-  it("should support rest parameter", () => {
-    function sum(...values) {
-      let total = 0;
-      for (const value of values) {
-        total += value;
-      }
-      return total;
-    }
-    expect(sum(1, 2, 3, 4, 5)).toBe(15);
-  });
-  it("should support optional parameter", () => {
-    function sayHello(firstName, lastName) {
-      if (lastName) {
-        return `Hello ${firstName} ${lastName}`;
-      }
-      return `Hello ${firstName}`;
-      // if (lastName) {
-      //   console.info(`Hello ${firstName} ${lastName}`);
-      // }
-      // console.info(`Hello ${firstName}`);
-    }
-    // console.info(sayHello("Sandi"));
-    // console.info(sayHello("Sandi", "Arba"));
-    expect(sayHello("Sandi")).toBe("Hello Sandi");
-    expect(sayHello("Sandi", "Arba")).toBe("Hello Sandi Arba");
-  });
-  it("should support function overloading", () => {
-    function callMe(value) {
-      if (typeof value === "string") {
-        return value.toUpperCase();
-      } else if (typeof value === "number") {
-        return value * 10;
-      }
-    }
-    expect(callMe(100)).toBe(1000);
-    expect(callMe("Sandi")).toBe("SANDI");
-  });
+    it("should support in typescript", () => {
+        function sayHello(name) {
+            return `Hello ${name}`;
+        }
+        expect(sayHello("Sandi")).toBe("Hello Sandi");
+        function printHello(name) {
+            console.info(`Hello ${name}`);
+        }
+        printHello("Sandi");
+    });
+    it("should support default value", () => {
+        function sayHello(name = "Guest") {
+            return `Hello ${name}`;
+        }
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Sandi")).toBe("Hello Sandi");
+    });
+    it("should support rest parameter", () => {
+        function sum(...values) {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+        expect(sum(1, 2, 3, 4, 5)).toBe(15);
+    });
+    it("should support optional parameter", () => {
+        function sayHello(firstName, lastName) {
+            if (lastName) {
+                return `Hello ${firstName} ${lastName}`;
+            }
+            return `Hello ${firstName}`;
+            // if (lastName) {
+            //   console.info(`Hello ${firstName} ${lastName}`);
+            // }
+            // console.info(`Hello ${firstName}`);
+        }
+        // console.info(sayHello("Sandi"));
+        // console.info(sayHello("Sandi", "Arba"));
+        expect(sayHello("Sandi")).toBe("Hello Sandi");
+        expect(sayHello("Sandi", "Arba")).toBe("Hello Sandi Arba");
+    });
+    it("should support function overloading", () => {
+        function callMe(value) {
+            if (typeof value === "string") {
+                return value.toUpperCase();
+            }
+            else if (typeof value === "number") {
+                return value * 10;
+            }
+        }
+        expect(callMe(100)).toBe(1000);
+        expect(callMe("Sandi")).toBe("SANDI");
+    });
+    it("should support function as parameter", () => {
+        function sayHello(name, filter) {
+            return `Hello ${filter(name)}`;
+        }
+        function toUpper(name) {
+            return name.toUpperCase();
+        }
+        expect(sayHello("Sandi", toUpper)).toBe("Hello SANDI");
+        // ini kalau mau pakai anonymous function, bisa juga langsung kaya begini
+        // ini function biasa
+        expect(sayHello("Sandi", function (name) {
+            return name.toUpperCase();
+        })).toBe("Hello SANDI");
+        // ini arrow function with return
+        expect(sayHello("Sandi", (name) => {
+            return name.toUpperCase();
+        })).toBe("Hello SANDI");
+        // ini arrow function without return
+        expect(sayHello("Sandi", (name) => name.toUpperCase())).toBe("Hello SANDI");
+    });
 });
---
+++ dist/tests/if.test.js
@@ -0,0 +1,15 @@
+"use strict";
+describe("If Statement", () => {
+    it("should support in typescript", () => {
+        const examValue = 90;
+        if (examValue > 80) {
+            console.info("Good");
+        }
+        else if (examValue > 60) {
+            console.info("Not Bad");
+        }
+        else {
+            console.info("Try Again");
+        }
+    });
+});
