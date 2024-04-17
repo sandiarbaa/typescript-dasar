92b9fab7511fb73f74e83601bdd6624351418518
sandiarbaa <sandiarbaa@gmail.com>
2024-04-17 10:00:33 +0700

    add: Function Parameter

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -11,4 +11,45 @@ describe("Function", () => {
 
     printHello("Sandi");
   });
+
+  it("should support default value", () => {
+    function sayHello(name: string = "Guest"): string {
+      return `Hello ${name}`;
+    }
+
+    expect(sayHello()).toBe("Hello Guest");
+    expect(sayHello("Sandi")).toBe("Hello Sandi");
+  });
+
+  it("should support rest parameter", () => {
+    function sum(...values: number[]): number {
+      let total = 0;
+
+      for (const value of values) {
+        total += value;
+      }
+
+      return total;
+    }
+
+    expect(sum(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  it("should support optional parameter", () => {
+    function sayHello(firstName: string, lastName?: string): string {
+      if (lastName) {
+        return `Hello ${firstName} ${lastName}`;
+      }
+      return `Hello ${firstName}`;
+      // if (lastName) {
+      //   console.info(`Hello ${firstName} ${lastName}`);
+      // }
+      // console.info(`Hello ${firstName}`);
+    }
+    // console.info(sayHello("Sandi"));
+    // console.info(sayHello("Sandi", "Arba"));
+
+    expect(sayHello("Sandi")).toBe("Hello Sandi");
+    expect(sayHello("Sandi", "Arba")).toBe("Hello Sandi Arba");
+  });
 });
